3b072178de74141faff0a025905b5523efe8577c
yuchengqian <yuchengqian@guoxiatech.com>
2026-04-22 15:39:40 +0800

    删除缓存

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /opt/ha/config/custom_components/hanchu-ess-ha
 git pull http://yuchengqian:password_1@git.guoxia.cloud/backend/ess/hanchu-ess-ha
+rm -rf __pycache__
 sleep 2
 docker restart homeassistant
